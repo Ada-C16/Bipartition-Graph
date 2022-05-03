f5871576c28823eb28a83aff5332b2f7e24d8fda
bailey <baileyholderman@gmail.com>
2022-05-03 15:40:31 -0700

    implements working possible_bipartition function, passes 7/7 tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,12 +1,33 @@
 # Can be used for BFS
-from collections import deque 
+from collections import deque
+from collections import defaultdict
 
-def possible_bipartition(dislikes):
+def possible_bipartition(dislikes: list[list[int]]):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
         into the same partition.
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    group_assignment = {i: None for i in range(len(dislikes))}
+
+    # dfs recursively
+
+    def dfs(node, group):
+        if not group_assignment[node]:
+            group_assignment[node] = group
+        else:
+            return group_assignment[node] == group
+
+        for dog in dislikes[node]:
+            if not dfs(dog, 2 if group == 1 else 1):
+                return False
+        return True
+
+    for i in range(len(dislikes)):
+        if not group_assignment[i] and not dfs(i, 1):
+            return False
+
+    return True
+
 
