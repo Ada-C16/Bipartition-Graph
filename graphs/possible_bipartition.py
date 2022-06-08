92c1e89b9fe2f9be062ce52192366e080f60a2b2
Shay <shay.phillips11@gmail.com>
2022-06-08 16:04:52 -0400

    passing all tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -5,8 +5,28 @@ def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
         into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(N + E) where N = number of nodes (or puppies) and E = number of edges (or dislike relationships)
+        Space Complexity: O(N)
     """
-    pass
+    
+    if len(dislikes) <= 2:
+        return True
 
+    stack = []
+    visited_and_parent = {}
+
+    for n in range(len(dislikes)):
+        if dislikes[n]!= []:
+            stack.append(n)
+            visited_and_parent[n] = None
+            break
+    
+    while stack:
+        current = stack.pop()
+        for neighbor in dislikes[current]:
+            if neighbor not in visited_and_parent:
+                visited_and_parent[neighbor] = current
+                stack.append(neighbor)
+            elif neighbor in visited_and_parent and neighbor != visited_and_parent[current]:
+                return False
+    return True
