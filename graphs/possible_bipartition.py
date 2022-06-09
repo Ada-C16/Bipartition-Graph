f9c66360034866dceb8527304ce71357a28036da
Michelle Bodart <michelle.bodart@gmail.com>
2022-06-09 15:20:43 -0700

    all tests passing

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -2,79 +2,42 @@
 from collections import deque 
 
 def possible_bipartition(dislikes):
-    """ Will return True or False if the given graph
-        can be bipartitioned without neighboring nodes put
-        into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
-    """
-    '''
-    They can be partitioned if there are no cycles
-    OR if there are no odd numbered cycles
-    STEPS:
-        do depth first search looking for cycles
-            like regular depth first exept you store what the return value is
-            if it has something that has been seen before that's not the return value
-            Then it's a cycle!
-
-    OR maybe I can just traverse either putting it in A or B, if I run into an issue then false
-    DFS 
-    UNION OF SETS - NOPE, go back to looking for loops
-    '''
-    
-
-    
-            
-
-def bfs(dislikes):
-    q = deque()
-    visited_nodes = set()
-    for i in range(len(dislikes)):  
-        if i not in visited_nodes:
-        # doing this to get any islands, without going through the whole thing unnecessarily
-            q.append(i)
-            while len(q) > 0:
-                current = q.popleft()
-                visited_nodes.add(current)
-                for node in dislikes[current]:
-                    if node not in visited_nodes:
-                        q.append(node)
-    return visited_nodes
-
-
-def dfs(dislikes):
-    visited = set()
+    # get the first dog that has dislikes, if there are none, return True
+    first = None
     for i in range(len(dislikes)):
-        if i not in visited:
-            dfs_helper(dislikes, visited, i)
+        if dislikes[i]:
+            first = i
+            break
+    if first == None:
+        return True
 
-def dfs_helper(dislikes, visited, current):
-    if current in visited:
-        return
-    else:
-        visited.add(current)
-        for node in dislikes[current]:
-            dfs_helper(dislikes, visited, node)
-
-def find_cycle(dislikes):
-    all_visited_nodes = set()
+    # initialize queue, two dog pens, add the first dog to the queue and assign it to pen_a
     q = deque()
-    for i in range(len(dislikes)):
-        visited_nodes_this_cycle = set()
-        if i not in all_visited_nodes:
-            q.append(i)
-            while len(q) > 0:
-                current = q.popleft()
-                all_visited_nodes.add(current)
-                visited_nodes_this_cycle.add(current)
-                for node in dislikes[current]:
-                    if node not in all_visited_nodes:
-                        q.append(node)
-                    elif node in visited_nodes_this_cycle and current not in dislikes[node]:
-                        return True
-    return False
+    pen_a = set()
+    pen_b = set()
+    q.append((first, "pen_a"))
 
+    while len(q) > 0:
+        # current = q.popleft() # breadth first
+        current = q.pop() # depth first - they both work
+        dog = current[0]
+        assigned_pen_name = current[1]
 
+        friendly_pen = pen_a if assigned_pen_name == "pen_a" else pen_b
+        enemy_pen = pen_b if assigned_pen_name == "pen_a" else pen_a
+        enemy_pen_name = "pen_b" if assigned_pen_name == "pen_a" else "pen_a"
 
+        # if current is in one pen and assigned to the other pen, return False
+        # if current is in one pen and assigned to that pen, do nothing
+        # otherwise, current is a new dog, so add it to its assigned pen and add its 
+        # dislikes to the que with the opposite assigned pen
+        if dog in enemy_pen:
+            return False
+        elif dog in friendly_pen:
+            continue
+        else:
+            friendly_pen.add(dog)
+            for dog in dislikes[dog]:
+                q.append((dog, enemy_pen_name))
 
-
+    return True
