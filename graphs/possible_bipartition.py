c9c062a05acbeb30dade2f7e68f3007225cb5630
syblades <86990570+syblades@users.noreply.github.com>
2022-06-11 16:24:28 -0700

    Added Big O

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -5,8 +5,30 @@ def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
         into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
+        N = Nodes + E = Edges
+        Time Complexity: O(N + E)
+        Space Complexity: O(N)
+
     """
-    pass
+    # color dict stores bool values
+    color = {}
+    for node in range(len(dislikes)):
+        if node not in color:
+            stack = [node]
+            color[node] = 0
+            while stack:
+                node = stack.pop()
+                for neighbor in dislikes[node]:
+                    if neighbor not in color:
+                        stack.append(neighbor)
+                        color[neighbor] = not(color[node])
+                    elif color[neighbor] == color[node]:
+                        return False
+    return True
+
+
+
+
+
+
 
