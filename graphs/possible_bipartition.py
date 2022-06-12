88b8a0bd7b88b54f3a1deb760001a05267e7e280
sarahstandish <z.sisyphusrocks@gmail.com>
2022-06-12 16:50:55 -0700

    passing all tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,5 +1,7 @@
 # Can be used for BFS
-from collections import deque 
+from collections import deque
+from dis import dis
+from pydoc import visiblename 
 
 def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
@@ -8,5 +10,33 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    visited = set()
+
+    l = len(dislikes)
+
+    if l == 0:
+        return True
+
+    def dfs(node, parent=None):
+
+        if node in visited:
+            return False
+        visited.add(node)
+        
+        for enemy in dislikes[node]:
+            if enemy != parent:
+                has_no_cycle = dfs(enemy, node)
+                if not has_no_cycle:
+                    return False
+        
+        return True
+
+
+    for dog in range(l):
+        if dog not in visited:
+            has_no_cycle = dfs(dog)
+            if not has_no_cycle:
+                return False
+    return True
+
 
