8e7410af5762f6a4043d29f85b047d429d22bf97
Lux Barker <lux.barker13@gmail.com>
2022-07-07 16:55:46 -0700

    pass possible_bipartition tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,5 +1,5 @@
 # Can be used for BFS
-from collections import deque 
+from collections import deque
 
 def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
@@ -8,5 +8,36 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if not dislikes:
+        return True
+
+    groups = [None] * len(dislikes)
+    GROUP_A = 0
+    GROUP_B = 1
+    start_dog = 0
+    # find first dog with dislikes
+    while start_dog < len(dislikes) and not dislikes[start_dog]:
+        start_dog += 1
+
+    groups[start_dog] = GROUP_A
+    queue = deque()
+    queue.append(start_dog)
+    while queue:
+        curr_dog = queue.popleft()
+        for disliked_dog in dislikes[curr_dog]:
+            # case: never seen this dog before
+            if groups[disliked_dog] is None:
+                # assign it to its group
+                groups[disliked_dog] = GROUP_A if groups[curr_dog] == GROUP_B else GROUP_B
+                queue.append(disliked_dog)
+            elif groups[disliked_dog] == groups[curr_dog]:
+                # graph can't be bipartitioned
+                return False
+    return True
+
+
+
+
+
+
 
