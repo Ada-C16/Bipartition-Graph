fd01e7e7491990dfe153875d34f636883f5c9347
Monica Cruz <mcatcruz@gmail.com>
2022-07-10 20:35:47 -0700

    Passing all tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -14,17 +14,29 @@ def possible_bipartition(dislikes):
     kennel_b = []
 
     # Add dogs to the queue
-    for dog in dislikes:
-        dog_queue.append(dog)
+    for i in range(len(dislikes)):
+        dislikes[i].append(i)
+        dog_queue.append(dislikes[i])
 
     while len(dog_queue) != 0:
     # Assign dog to check to current_dog
-        current_dog = dog_queue.pop()
+        current_dog = dog_queue.popleft()
 
-        if current_dog not in kennel_a:
-            kennel_a.append(current_dog)
-        elif current_dog not in kennel_b:
+        if len(kennel_a) == 0:
+            kennel_a.append(current_dog.pop())
+        elif len(kennel_a) > 0 and len(kennel_b) == 0:
             kennel_b.append(current_dog)
+        else:
+            for dog in kennel_a:
+                if current_dog[0] != dog and current_dog[1] != dog:
+                    kennel_a.append(current_dog)
+                    break
+        # elif len(kennel_b) == 0:
+        #     kennel_b.append(current_dog)
+        if len(kennel_b) > 0:
+            for dog in kennel_b:
+                if current_dog[0] != dog and current_dog[1] != dog:
+                    kennel_b.append(current_dog)
         else:
             return False
 
