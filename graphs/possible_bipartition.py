730a41d8e2b7c8a57c27ea749113c97fe5709e4c
Ana Gabriele <gabbanasoares@gmail.com>
2022-07-11 22:48:59 -0700

    delete white space

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -58,7 +58,6 @@ Explanation: group1 [0, 1, 4], group2 [2, 3]
     print(graph)
     # BFS -> loop thorugh each item and check if items are enemies 
 
-    
     def bfs(queue, is_visited, node):
         queue.put((node,0))
         while not queue.empty():
