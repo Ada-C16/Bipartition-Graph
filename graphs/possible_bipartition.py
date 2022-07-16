3c343033436e6ea1c3550cd5b61b03ce2de6af05
lizett <lizett503@gmail.com>
2022-07-16 16:53:52 -0700

    Tests passing.

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -8,5 +8,39 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    group_map = {}
+
+    if not dislikes:
+        return True
+
+    first_dog = 0
+    for i in range(len(dislikes)):
+        if dislikes[i]:
+            first_dog = i
+            break
+    
+    # BFS
+    q = deque()
+    q.append(first_dog)
+    group_map[first_dog] = "group1"
+
+    while q:
+        current_dog = q.popleft()
+        enemies = dislikes[current_dog]
+
+        if group_map[current_dog] == "group1":
+            current_group_id = "group2"
+        else:
+            current_group_id = "group1"
+
+        for enemy in enemies:
+            if enemy in group_map:
+                if group_map[enemy] != current_group_id:
+                    return False
+            else:
+                group_map[enemy] = current_group_id
+                q.append(enemy)
+
+    return True
+
 
