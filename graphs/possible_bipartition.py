66a5c1d4541ce454d7d1b75820f3133713b37a0e
Diana <diana.lozanocalderon@gmail.com>
2022-07-20 13:53:00 -0700

    All tests pass

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,5 +1,6 @@
 # Can be used for BFS
-from collections import deque 
+from collections import deque
+import queue 
 
 def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
@@ -8,5 +9,23 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if not dislikes:
+        True
 
+    pack = {}
+    dogque = deque()
+
+    for dog in range(len(dislikes)):
+        if dog not in pack:
+            dogque += [dog]
+            pack[dog] = 0
+
+        while dogque:
+            doggy_node = dogque.popleft()
+            for neighbor in dislikes[doggy_node]:
+                if neighbor not in pack:
+                    dogque += [neighbor]
+                    pack[neighbor] = pack[doggy_node] ^ 1
+                elif pack[neighbor] == pack[doggy_node]:
+                    return False
+    return True
