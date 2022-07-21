2f0cdb6efaa9d1a984b5edee65376a4b552b3b89
sjolivas <sjolivas@gmail.com>
2022-07-21 17:04:58 -0600

    all tests passing

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -10,3 +10,37 @@ def possible_bipartition(dislikes):
     """
     if not dislikes:
         return True
+
+    first = 0
+    for dog in range(len(dislikes)):
+        if dislikes[dog]:
+            first = dog
+            break
+
+    checked_dogs = set()
+    checked_dogs.add(first)
+    group_a = []
+    group_b = []
+
+    q = deque()
+    q.append(first)
+
+    while q:
+        current = q.popleft()
+        checked_dogs.add(current)
+        if current not in group_a and current not in group_b:
+            group_a.append(current)
+        for solo_dog in dislikes[current]:
+            if solo_dog not in checked_dogs:
+                q.append(solo_dog)
+
+            if current in group_a:
+                if solo_dog in group_a:
+                    return False
+                group_b.append(solo_dog)
+            else:
+                if solo_dog in group_b:
+                    return False
+                group_a.append(solo_dog)
+
+    return True
