ce11bdcf5f2fa8db5119802e01049471bab6be69
Monica Cruz <mcatcruz@gmail.com>
2022-07-21 17:55:37 -0700

    Passing five of seven tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -5,8 +5,8 @@ def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
         into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n^2)
+        Space Complexity: O(n)
     """
     # Create an empty queue
     dog_queue = deque()
@@ -15,30 +15,22 @@ def possible_bipartition(dislikes):
 
     # Add dogs to the queue
     for i in range(len(dislikes)):
-        dislikes[i].append(i)
         dog_queue.append(dislikes[i])
 
     while len(dog_queue) != 0:
     # Assign dog to check to current_dog
-        current_dog = dog_queue.popleft()
+        current_dog_dislikes = dog_queue.popleft()
+        current_dog = dislikes.index(current_dog_dislikes)
 
-        if len(kennel_a) == 0:
-            kennel_a.append(current_dog.pop())
-        elif len(kennel_a) > 0 and len(kennel_b) == 0:
-            kennel_b.append(current_dog)
+        if len(current_dog_dislikes) == 0:
+            kennel_a.append(current_dog)
         else:
-            for dog in kennel_a:
-                if current_dog[0] != dog and current_dog[1] != dog:
+            for i in range(len(current_dog_dislikes)-1):
+                if current_dog_dislikes[i] not in kennel_a:
                     kennel_a.append(current_dog)
-                    break
-        # elif len(kennel_b) == 0:
-        #     kennel_b.append(current_dog)
-        if len(kennel_b) > 0:
-            for dog in kennel_b:
-                if current_dog[0] != dog and current_dog[1] != dog:
+                elif current_dog_dislikes[i] not in kennel_b:
                     kennel_b.append(current_dog)
-        else:
-            return False
-
+                else:
+                    return False
+    
     return True
-
