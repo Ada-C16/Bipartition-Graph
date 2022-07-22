3d0bbbae2118cbf1d807e2a677e7258f4b6662fd
Sabrina Lauredan <slauredan@gmail.com>
2022-07-21 20:37:53 -0400

    All tests passing.

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -8,5 +8,35 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    queue = deque()
+    set_a = set()
+    set_b = set()
+    visited = set()
+
+    if not dislikes:
+        return True
+    if dislikes[0]:
+        starting_node = 0
+    else:
+        starting_node = 1
+    
+    queue.append(starting_node)
+    visited.add(starting_node)
+    set_a.add(starting_node)
+
+    while len(queue) > 0:
+        node = queue.popleft()
+        for i in dislikes[node]:
+            if i not in visited:
+                queue.append(i)
+                visited.add(i)
+                if node in set_a:
+                    set_b.add(i)
+                else:
+                    set_a.add(i)
+            else:
+                if node in set_a and i in set_a or node in set_b and i in set_b:
+                    return False
+    
+    return True
 
