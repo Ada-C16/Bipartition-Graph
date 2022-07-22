03f31ca3988fb2604fb7603b2a4644d4ed373f72
tt-ht <ttht@protonmail.com>
2022-07-21 23:44:00 -0700

    implements possible_bipartition method

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -5,8 +5,36 @@ def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
         into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n+e)
+        Space Complexity: O(n)
     """
-    pass
+    if not dislikes:
+        return True
 
+    tracked = [False] * len(dislikes)
+    gargy_a = set()
+    gargy_b = set()
+
+    q = deque()
+    q.append(0)
+
+    while q:
+        current = q.popleft()
+        tracked[current] = True        
+        if not dislikes[current]:
+            q.append(current+1)
+
+        for gargoyle in dislikes[current]:
+            if not tracked[gargoyle]:              
+                q.append(gargoyle)
+
+            if current not in gargy_a:
+                if gargoyle in gargy_b:
+                    return False
+                gargy_a.add(gargoyle)    
+            else:
+                if gargoyle in gargy_a:
+                    return False
+                gargy_b.add(gargoyle)    
+    
+    return True
